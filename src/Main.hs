881fd0a2f7c18d4849427949095cb14ff13a88b7
Filip Brcic <brcha@yandex.com>
2021-06-30 16:33:17 +0200

    Fixed time formatting

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Data.Time (timeOfDayToTime)
+import           Data.Time
 
 main :: IO ()
 main = do
@@ -10,6 +10,7 @@ main = do
     times_list = map read times_list_str
     diffTimes_list = map timeOfDayToTime times_list
     sum_of_times = sum diffTimes_list
+    sum_str = formatTime defaultTimeLocale "%d days %0H:%0M:%0S" sum_of_times
 
-  print $ "Sum of times is: " ++ show sum_of_times
+  print $ "Sum of times is: " ++ sum_str
 

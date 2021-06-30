c85410106673c134bf4f9f7ad5cae366d7716018
Filip Brcic <brcha@yandex.com>
2021-06-30 16:17:26 +0200

    Finished the program

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import           Data.Time (timeOfDayToTime)
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  times_str <- getContents
+  let
+    times_list_str = words times_str
+    times_list = map read times_list_str
+    diffTimes_list = map timeOfDayToTime times_list
+    sum_of_times = sum diffTimes_list
+
+  print $ "Sum of times is: " ++ show sum_of_times
+

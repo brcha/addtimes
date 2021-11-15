a1ed9a2d2a358295aca66e7c212f2626fb91eaff
Filip Brcic <brcha@yandex.com>
2021-11-15 14:34:51 +0100

    Pad hours with zeros as well

--- src/Main.hs
+++ src/Main.hs
@@ -17,5 +17,8 @@ main = do
     minutes = todMin tod
     seconds = floor $ todSec tod :: Int
 
-  fmtLn $ "Sum of times is: " +|hours|+ ":" +|padLeftF 2 '0' minutes|+ ":" +|padLeftF 2 '0' seconds|+ ""
+  fmtLn $ "Sum of times is: " +|
+            padLeftF 2 '0' hours|+ ":" +|
+            padLeftF 2 '0' minutes|+ ":" +|
+            padLeftF 2 '0' seconds|+ ""
 
